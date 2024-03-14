a0e150b4f9a8b8eb96f7c96cf7c668d927539c3d
omniagaber55 <omniagaber@gmailcom>
2024-03-14 16:47:22 +0200

    first commit

---
+++ app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:app/constants.dart';
+import 'package:app/veiws/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: PrimaryColor),
+        useMaterial3: true,
+      ),
+      home: Onboarding_screen(),
+    );
+  }
+}
+
---
+++ app/lib/veiws/onboarding_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Onboarding_screen extends StatefulWidget {
+  const Onboarding_screen({super.key});
+
+  @override
+  State<Onboarding_screen> createState() => _Onboarding_screenState();
+}
+
+class _Onboarding_screenState extends State<Onboarding_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
